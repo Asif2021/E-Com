f4de70312940206a40c4968660612201cae884e2
ramzan617 <asiframzan2021@gmail.com>
2025-08-26 21:49:58 +0500

    refactor layout component to use React.Fragment for consistency

--- app/customerPage/page.jsx
+++
@@ -1,9 +0,0 @@
-import Shipping from '../components/customer/Shipping'
-const page = () => {
-  return (
-    <div>
-        <Shipping/>
-    </div>
-  )
-}
-export default page
--- app/sellerPage/layout.js
+++ app/sellerPage/layout.js
@@ -1,12 +1,12 @@
-import Header from '../components/Header'
+import React from 'react';
 import SideNav from '../components/customer/Sidenav';
 
 
 // export const experimental_ppr = true;
  
 export default function Layout({ children }) {
-  return (
-    <>
+  return (  
+    <React.Fragment>
     <div className='h-screen'>
      <div className="flex flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
@@ -15,7 +15,7 @@ export default function Layout({ children }) {
       <div className="flex-grow py-4 md:overflow-y-auto">{children}</div>
     </div>
     </div>
-    </>
+    </React.Fragment>
 
   );
 }
